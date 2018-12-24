ca50b9d1feccc2034e4f4a44d39d5a8ace618fea
vagrant <vagrant@homestead>
2018-12-24 14:43:42 +0000

    基础布局

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,9 +5,17 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     <title>@yield('title','Weibo App') - Laravel 新手入门教程</title>
 </head>
 <body>
-    @yield('content')
+    @include('layouts._header')
+
+    <div class="container">
+        <div class="offset-md-1 col-md-10">
+            @yield('content')
+            @include('layouts._footer')
+        </div>
+    </div>
 </body>
 </html>
