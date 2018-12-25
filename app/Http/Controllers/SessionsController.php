3317ce97ec60f1172fe800d69076962b0baf25d2
vagrant <vagrant@homestead>
2018-12-25 03:14:42 +0000

    记住我

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -20,7 +20,7 @@ class SessionsController extends Controller
         ]);
 
         //判断密码与邮箱是否匹配
-        if(Auth::attempt($credentials)){
+        if(Auth::attempt($credentials,$request->has('remember'))){
             session()->flash('success','欢迎回来！~');
             return redirect()->route('users.show',[Auth::user()]);
         }else{
