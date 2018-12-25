1c1ad4baa467b6c9b188bc47c77eafc0025ca701
vagrant <vagrant@homestead>
2018-12-25 03:09:23 +0000

    用户退出

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -28,4 +28,11 @@ class SessionsController extends Controller
             return redirect()->back()->withInput();
         }
     }
+
+    //用户退出逻辑
+    public function destroy(){
+        Auth::logout();
+        session()->flash('success','您已成功退出！');
+        return redirect('login');
+    }
 }
