e807d6b92183f07d6f88658021a8e8c20c3698c1
vagrant <vagrant@homestead>
2018-12-26 11:59:18 +0000

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -53,4 +53,37 @@ class User extends Authenticatable
         return $this->statuses()
                     ->orderBy('created_at','desc');
     }
+
+    //模型关联 粉丝关系
+    public function followers(){
+        return $this->belongsToMany(User::class,'followers','user_id','follower_id');
+    }
+
+    //模型关联 关注关系
+    public function followings(){
+        return $this->belongsToMany(User::class,'followers','follower_id','user_id');
+    }
+
+    //关注逻辑
+    public function follow($user_ids){
+        if (!is_array($user_ids)){
+            $user_ids = compact($user_ids);
+        }
+
+        $this->followings()->sync($user_ids,false);
+    }
+
+    //取消关注逻辑
+    public function unfollow($user_ids){
+        if (!is_array($user_ids)){
+            $user_ids = compact($user_ids);
+        }
+
+        $this->followings()->detach($user_ids);
+    }
+
+    //判断是否关注
+    public function isFollowing($user_id){
+        return $this->followings->contains($user_id);
+    }
 }
